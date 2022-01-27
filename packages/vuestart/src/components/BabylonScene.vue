870fd8d9304fee44ab61b14e7270c4c545cee3ed
zhangchao <544262408@qq.com>
2022-01-27 15:01:50 +0800

    .

--- packages/vuestart/src/components/BabylonScene.vue
+++ packages/vuestart/src/components/BabylonScene.vue
@@ -1,24 +1,76 @@
 <template>
-  <canvas ref="bjsCanvas" width="500" height="500" />
+  <canvas ref="bjsCanvas" />
 </template>
 <script>
 import { ref, onMounted } from "@vue/runtime-core";
 import { createScene } from "./MyFirstScene";
 
+var myScene;
 export default {
   name: "BabylonScene",
+  props: {
+    width: {
+      type: String,
+      default: () => {
+        return document.documentElement.clientWidth+"px";
+      },
+    },
+    height: {
+      type: String,
+      default: () => {
+        return document.documentElement.clientHeight+"px";
+      },
+    },
+    name:{
+      type:String,
+      default: () => {
+        return '';
+      }
+    },
+    m3dName:{
+      type:String,
+      default: () => {
+        return '';
+      }
+
+    }
+  },
   setup() {
     const bjsCanvas = ref(null);
+    bjsCanvas.height = document.documentElement.clientHeight;
 
     onMounted(() => {
       if (bjsCanvas.value) {
-        createScene(bjsCanvas.value);
+        bjsCanvas.value.width = document.documentElement.clientWidth;
+        bjsCanvas.value.height = document.documentElement.clientHeight;
+        
+      myScene =  createScene(bjsCanvas.value);
       }
     });
 
     return {
       bjsCanvas,
     };
+  },
+  mounted() {
+    window.addEventListener('resize', this.getDimensions);
+  },
+  methods: {
+    getDimensions() {
+      const bjsCanvas = this.$refs.bjsCanvas;
+      
+      if(bjsCanvas){
+        console.log(document.documentElement.clientWidth,document.documentElement.clientHeight);
+        bjsCanvas.style.width = document.documentElement.clientWidth+"px";
+        bjsCanvas.style.height = document.documentElement.clientHeight+"px";
+        bjsCanvas.width = document.documentElement.clientWidth;
+        bjsCanvas.height = document.documentElement.clientHeight;
+        myScene.getEngine().resize();
+
+      }
+      
+      
+    }
   },
 };
 </script>
--- packages/vuestart/src/components/MyFirstScene.js
+++ packages/vuestart/src/components/MyFirstScene.js
@@ -1,5 +1,6 @@
 import { Engine, Scene, FreeCamera, Vector3, MeshBuilder, StandardMaterial,
      Color3, HemisphericLight } from "@babylonjs/core";
+import { AdvancedDynamicTexture,Image } from '@babylonjs/gui/2D';
      
 const createScene = (canvas) => {
   const engine = new Engine(canvas);
@@ -16,9 +17,17 @@ const createScene = (canvas) => {
   material.diffuseColor = Color3.Blue();
   box.material = material;
 
+  const advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI("UI");
+    const image = new Image("CrossHair", "https://playground.babylonjs.com/textures/sand.jpg");
+    image.width = "30px";
+    image.height = "30px";
+    image.stretch = Image.STRETCH_NONE;
+    advancedTexture.addControl(image);  
+
   engine.runRenderLoop(() => {
     scene.render();
   });
+  return scene;
 };
 
 export { createScene };
